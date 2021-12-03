eaad4de1681411aa4e3c75961af72c896ed1f716
Rizki Putra Julianto <rizkiputrajulianto111@gmail.com>
2021-12-03 19:54:46 +0700

    test

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const { sequelize, Sequelize } = require('./src/models');
+const router = require('./src/routes');
+const cors = require('cors');
 require('dotenv').config();
 
-app.set('port', process.env.port || 3000);
+// sequelize.sync({ force: true });
+// sequelize.sync();
 
-app.get('/', (req, res, next) => {
-	res.send('<h1>Hello world<h1>');
-});
+var app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static('uploads'));
+app.use(router);
+
+// app.listen(process.env.PORT || 5000);
 
 module.exports = app;
