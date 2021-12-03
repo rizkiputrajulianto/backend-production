2c9fd7c0e008e8a045f64dac03124948250c5c58
Rizki Putra Julianto <rizkiputrajulianto111@gmail.com>
2021-12-03 19:58:25 +0700

    urlencode

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static('uploads'));
