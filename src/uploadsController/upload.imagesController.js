3f2f94ba7ba58e3b7e66a291d752557aeb1e1843
Rizki Putra Julianto <rizkiputrajulianto111@gmail.com>
2021-12-04 10:13:56 +0700

    adding test checkpoint

--- src/uploadsController/upload.imagesController.js
+++ src/uploadsController/upload.imagesController.js
@@ -2,8 +2,8 @@ const service = async (req, res, next) => {
 	try {
 		const descFile = req.file.path;
 		const splitFile = descFile.split('\\');
-		// const link = 'http://localhost:3000/' + splitFile.join('/');
-		const link = 'https://backend-finals.herokuapp.com/' + splitFile.join('/');
+		const link = 'http://localhost:3000/' + splitFile.join('/');
+		// const link = 'https://backend-finals.herokuapp.com/' + splitFile.join('/');
 		return res.json({ data: link });
 	} catch (error) {
 		return res.status(500).json({ msg: error.toString() });
